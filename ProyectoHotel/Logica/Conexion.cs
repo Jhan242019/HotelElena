f11bea43a04824d55e9a5a7e2a7330c5f7860e16
Jhan242019 <jahn24@hotmail.com>
2022-03-01 22:06:07 -0500

    Completo

--- ProyectoHotel/Logica/Conexion.cs
+++ ProyectoHotel/Logica/Conexion.cs
@@ -7,6 +7,8 @@ namespace ProyectoHotel.Logica
 {
     public class Conexion
     {
-        public static string CN = "Data Source=JHANPC\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+        //public static string CN = "Data Source=tcp:serverhotel.database.windows.net,1433;Initial Catalog=Hotel;User Id=administrador@serverhotel;Password=JC2406962020jc2021";
+        public static string CN = "Data Source=JHANPC\\SQLEXPRESS;Database=DB_HOTEL;User Id=sa;Password=12345678;Trusted_Connection=False;MultipleActiveResultSets=True";
+        //public static string CN = "Data Source=JHANPC\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
     }
 }
